878d9eedb88c0a0c0391da7a13dfba34158514a7
ggdio <ggrdio@gmail.com>
2013-09-25 12:09:16 -0300

    AS jaxws implementation

---
+++ soa-cap04-tomcat/src/main/java/br/com/ggdio/estoque/model/Autor.java
@@ -0,0 +1,24 @@
+package br.com.ggdio.estoque.model;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Autor {
+	
+	private String nome;
+
+	public Autor(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+}
